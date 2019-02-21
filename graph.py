b491552ccf1090f5661a06da6cc57659c535d228
Venmugil <venmugilelango@baidu.com>
2019-02-21 13:12:37 -0800

    Added maxpool and conv classes

--- graph.py
+++ graph.py
@@ -11,6 +11,12 @@ import operator as op
 peak_flop = float(15 * 1000) # 125 TFLOP
 bw = float(150 / 8) # 150 GBytes/sec = 7/8 GWords/sec
 bw_to_flop = float(peak_flop / bw)
+pw_ops_in_bn = 9 # No. of pointwise ops in a batch-norm
+
+
+def factors(n):    
+    return set(reduce(list.__add__, ([i, n//i] for i in range(1, int(n**0.5) +
+        1) if n % i == 0)))
 
 
 def RowCartesian(arr1, arr2):
@@ -76,17 +82,39 @@ def GetEdgeCosts(src_op, tgt_op):
 def GetNodeConfigs(node_dom, n_procs):
     dim = len(node_dom)
     log_n_procs = int(math.log2(n_procs))
-    procs = [1 << i for i in range(log_n_procs + 1)]
 
-    configs = [c for c in itertools.product(procs, repeat=dim) if reduce(op.mul,
-        c, 1) <= n_procs]
+    proc_set = []
+    for d in node_dom:
+        proc_set.append(factors(d))
+
+    configs = [c for c in itertools.product(*proc_set) if reduce(op.mul, c, 1)
+        <= n_procs]
     return configs
 
 
+# Converts convolution domain to GEMM
+def ConvToGemm(img, fltr, stride, pad):
+    assert(len(img) == 4)
+    assert(len(fltr) == 4)
+    assert(img[1] == fltr[1])
+
+    c, h, w = img[1], img[2], img[3]
+    r, s = fltr[2], fltr[3]
+
+    h_o = int((h - r + 2*pad) / stride) + 1
+    w_o = int((w - s + 2*pad) / stride) + 1
+
+    k = c * r * s
+    n = fltr[0]
+    m = img[0] * h_o * w_o
+
+    return (m, n, k)
+
+
 class Gemm():
-    def __init__(self, dom_size, n_procs, pw_op=False):
+    def __init__(self, dom_size, n_procs, pw_op_cnt=0):
         assert(len(dom_size) == 3)
-        self.pw_op = pw_op
+        self.pw_op_cnt = pw_op_cnt
 
         m_idx, n_idx, k_idx = 0, 1, 2
 
@@ -110,9 +138,19 @@ class Gemm():
         dom_per_proc = np.ceil(self.dom / self.dom_configs)
         costs = 3.0 * np.prod(dom_per_proc, axis=1)
 
+        # Matrix addition cost for weight update
+        update_cost = dom_per_proc[:, k_dim] * dom_per_proc[:, n_dim]
+        costs += update_cost
+
         # Cost of pointwise op
-        if self.pw_op == True:
-            costs += (dom_per_proc[:, m_dim] * dom_per_proc[:, n_dim])
+        if self.pw_op_cnt > 0:
+            pw_cost = dom_per_proc[:, m_dim] * dom_per_proc[:, n_dim]
+            costs += self.pw_op_cnt * 3 * pw_cost # Factor 3 is to account for 1
+                                                  # pointwise op (per element)
+                                                  # in fwd phase, 1
+                                                  # differentiation op in bwd
+                                                  # phase, and 1 hadamard
+                                                  # product in bwd phase
     
         # Cost for reducing the output during fwd phase
         # All-reduce cost = 2*((m*n)/P)*(P-1)
@@ -130,6 +168,83 @@ class Gemm():
         steps = 2.0 * (procs - 1) # When procs = 1, the cost is 0
         costs += (bw_to_flop * (words * steps))
     
+        return costs
+
+
+class MaxPool():
+    def __init__(self, fltr, stride):
+        assert(len(fltr) == 2)
+
+        self.r = fltr[0]
+        self.s = fltr[1]
+        self.stride = stride
+
+
+class Conv():
+    def __init__(self, img, fltr, stride, pad, n_procs, maxpool=None, pw_op_cnt=0):
+        assert(len(img) == 4)
+        assert(len(fltr) == 4)
+        assert(img[1] == fltr[1])
+
+        self.n_procs = n_procs
+        self.maxpool = maxpool
+        self.pw_op_cnt = pw_op_cnt
+
+        m_idx, n_idx, k_idx = 0, 1, 2
+
+        b, n = img[0], fltr[0]
+        c, h, w = img[1], img[2], img[3]
+        r, s = fltr[2], fltr[3]
+
+        h_o = int((h - r + 2*pad) / stride) + 1
+        w_o = int((w - s + 2*pad) / stride) + 1
+
+        # Domain
+        self.dom = ConvToGemm(img, fltr, stride, pad)
+
+        # Reduced domain when maxpool is applied to 'img'. Configurations are
+        # created on this reduced domain so that there is no intra-op
+        # communication for max-pooling.
+        if maxpool:
+            self.maxpool.h_o = int((h_o - maxpool.r) / maxpool.stride) + 1
+            self.maxpool.w_o = int((w_o - maxpool.s) / maxpool.stride) + 1
+
+            maxpool_m = b * self.maxpool.h_o * self.maxpool.w_o
+            self.maxpool.dom = (maxpool_m, n)
+            dom_with_maxpool = [maxpool_m] + self.dom[1:]
+        else:
+            dom_with_maxpool = self.dom
+
+        # Input/output tensors
+        self.in_tsr = (self.dom[m_idx], self.dom[k_idx])
+        if maxpool:
+            self.out_tsr = self.maxpool.dom
+            self.orig_out_tsr = (b, n, self.maxpool.h_o, self.maxpool.w_o)
+        else:
+            self.out_tsr = (self.dom[m_idx], self.dom[n_idx])
+            self.orig_out_tsr = (b, n, h_o, w_o)
+
+        # Configurations
+        self.dom_config_tuples = GetNodeConfigs(dom_with_maxpool, n_procs)
+        self.dom_configs = np.array(self.dom_config_tuples)
+        self.in_tsr_configs = self.dom_configs[:, (m_idx, k_idx)]
+        self.out_tsr_configs = self.dom_configs[:, m_idx:n_idx+1]
+        if maxpool:
+            self.maxpool.dom_configs = self.dom_configs[:, 0:2]
+
+
+    def GetVertexCosts(self):
+        gemm_op = Gemm(self.dom, self.n_procs, self.pw_op_cnt)
+        costs = gemm_op.GetVertexCosts()
+
+        # Add the cost for max-pooling
+        if self.maxpool:
+            dom_per_proc = self.maxpool.dom / self.maxpool.dom_configs
+            maxpool_costs = 3.0 * np.prod(dom_per_proc, axis=1)
+
+            assert(costs.shape == maxpool_costs.shape)
+            costs += maxpool_costs
+
         return costs
 
 
@@ -159,82 +274,66 @@ def AssignCostsToEdges(G):
         edge_attr['costs'] = pd.Series(costs, index=idx, name='cost')
 
 
-# Converts convolution domain to GEMM
-def ConvToGemm(img, fltr, stride, pad):
-    assert(len(img) == 4)
-    assert(len(fltr) == 4)
-    assert(img[1] == fltr[1])
-
-    c = img[1]
-    h = img[2]
-    w = img[3]
-    r = fltr[2]
-    s = fltr[3]
-
-    h_o = int((h - r + 2*pad) / stride) + 1
-    w_o = int((w - s + 2*pad) / stride) + 1
-
-    k = c * r * s
-    n = fltr[0]
-    m = img[0] * h_o * w_o
-
-    return [m, n, k]
-
-
 def Alexnet(G, b, n_procs):
-    img = [b, 3, 227, 227]
+    img = (b, 3, 227, 227)
     node_id = 0
 
-    # Conv1
-    dom = ConvToGemm(img, [96, 3, 11, 11], 4, 0)
-    G.add_node(node_id, dim=len(dom), dom=dom)
+    # Conv1 + relu + maxpool + norm
+    dom = ConvToGemm(img, (96, 3, 11, 11), 4, 0)
+    node_op = Conv(img, (96, 3, 11, 11), 4, 0, n_procs, MaxPool((3,3), 2),
+            pw_ops_in_bn+1)
+    G.add_node(node_id, op=node_op)
     node_id += 1
 
-    # TODO: maxpool
-
-    # Conv2
-    dom = ConvToGemm(dom, [256, 96, 5, 5], 1, 2)
-    G.add_node(node_id, dim=len(dom), dom=dom)
+    # Conv2 + relu + maxpool + norm
+    dom = ConvToGemm(dom, (256, 96, 5, 5), 1, 2)
+    node_op = Conv(node_op.orig_out_tsr, (256, 96, 5, 5), 1, 2, n_procs,
+            MaxPool((3,3), 2), pw_ops_in_bn+1)
+    G.add_node(node_id, op=node_op)
     G.add_edge(node_id - 1, node_id)
     node_id += 1
 
-    # TODO: maxpool
-
-    # Conv3
-    dom = ConvToGemm(dom, [384, 256, 3, 3], 1, 1)
-    G.add_node(node_id, dim=len(dom), dom=dom)
+    # Conv3 + relu
+    dom = ConvToGemm(dom, (384, 256, 3, 3), 1, 1)
+    node_op = Conv(node_op.orig_out_tsr, (384, 256, 3, 3), 1, 1, n_procs,
+            pw_op_cnt=1)
+    G.add_node(node_id, op=node_op)
     G.add_edge(node_id - 1, node_id)
     node_id += 1
 
-    # Conv4
-    dom = ConvToGemm(dom, [384, 384, 3, 3], 1, 1)
-    G.add_node(node_id, dim=len(dom), dom=dom)
+    # Conv4 + relu
+    dom = ConvToGemm(dom, (384, 384, 3, 3), 1, 1)
+    node_op = Conv(node_op.orig_out_tsr, (384, 384, 3, 3), 1, 1, n_procs,
+            pw_op_cnt=1)
+    G.add_node(node_id, op=node_op)
     G.add_edge(node_id - 1, node_id)
     node_id += 1
 
-    # Conv5
-    dom = ConvToGemm(dom, [256, 384, 3, 3], 1, 1)
-    G.add_node(node_id, dim=len(dom), dom=dom)
+    # Conv5 + relu + maxpool
+    dom = ConvToGemm(dom, (256, 384, 3, 3), 1, 1)
+    node_op = Conv(node_op.orig_out_tsr, (256, 384, 3, 3), 1, 1, n_procs,
+            MaxPool((3,3), 2), 1)
+    G.add_node(node_id, op=node_op)
     G.add_edge(node_id - 1, node_id)
     node_id += 1
 
-    # TODO: maxpool
+    # TODO: Reshape
 
-    # FC1
-    dom = [b, 4096, 256*6*6]
-    G.add_node(node_id, dim=len(dom), dom=dom)
+    # FC6 + relu
+    node_op = Gemm(node_op.out_tsr, (node_op.out_tsr[-1], 4096), n_procs, 1)
+    G.add_node(node_id, op=node_op)
     G.add_edge(node_id - 1, node_id)
     node_id += 1
 
-    # FC2
-    dom = [b, 4096, 4096]
-    G.add_node(node_id, dim=len(dom), dom=dom)
+    # FC7 + relu
+    node_op = Gemm(node_op.out_tsr, (node_op.out_tsr[-1], 4096), n_procs, 1)
+    G.add_node(node_id, op=node_op)
     G.add_edge(node_id - 1, node_id)
     node_id += 1
 
-    # FC3
-    dom = [b, 1024, 4096]
-    G.add_node(node_id, dim=len(dom), dom=dom)
+    # FC8 + relu
+    node_op = Gemm(node_op.out_tsr, (node_op.out_tsr[-1], 1024), n_procs, 1)
+    G.add_node(node_id, op=node_op)
     G.add_edge(node_id - 1, node_id)
     node_id += 1
 
