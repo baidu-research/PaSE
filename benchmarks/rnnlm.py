7087cb65716456776b365ad14f3ea11d444c28ff
Venmugil <venmugilelango@baidu.com>
2020-02-28 19:33:24 -0800

    Updates to rnnlm_data to use mtf for embedding and loss layers

--- benchmarks/rnnlm.py
+++ benchmarks/rnnlm.py
@@ -6,12 +6,13 @@ import utils
 from dataloader import TextDataLoader
 
 class Params():
-    def __init__(self, batch_size, vocab_size, max_seq_len, devices):
+    def __init__(self, batch_size, vocab_size, max_seq_len, num_nodes, devices):
         self.batch_size = batch_size
         self.num_units = 2048
         self.vocab_size = vocab_size
         self.max_seq_len = max_seq_len
         self.num_layers = 2
+        self.num_nodes = num_nodes
         self.devices = devices
 
 def main():
@@ -28,12 +29,13 @@ def main():
 
     vocab_size = utils.RoundUp(dataset.src_vocab_size, 8)
     print("Vocab size: %d" % vocab_size)
-    params = Params(args.batch_size, vocab_size, args.seq_len, devices)
+    params = Params(args.batch_size, vocab_size, args.seq_len,
+            trainer.num_nodes, devices)
 
     # Model
     if args.strategy == 0:
         import rnnlm_data
-        loss, grads = rnnlm_data.model(params, inputs, labels)
+        init_ops, loss, grads = rnnlm_data.model(params, inputs, labels)
     elif args.strategy == 1:
         import rnnlm_opt
         loss, grads = rnnlm_opt.model(params, inputs, labels)
@@ -47,11 +49,10 @@ def main():
         assert False
 
     # Train
-    run_options = tf.RunOptions(report_tensor_allocations_upon_oom = True)
-    config = tf.ConfigProto(log_device_placement=True,
-            allow_soft_placement=True)
-    trainer.train(tf.global_variables_initializer(), loss, [grads], dataset,
-            config=config, run_options=run_options)
+    run_options = tf.RunOptions(report_tensor_allocations_upon_oom=True)
+    config = tf.ConfigProto(allow_soft_placement=False)
+    trainer.train(init_ops, loss, [grads], dataset, config=config,
+            run_options=run_options)
 
 
 if __name__ == '__main__':
--- benchmarks/rnnlm_data.py
+++ benchmarks/rnnlm_data.py
@@ -1,5 +1,46 @@
+import numpy as np
 import tensorflow.compat.v1 as tf
 import tensorflow.keras as keras
+import mesh_tensorflow as mtf
+import utils
+
+def GetShape(dims):
+    sh = []
+    for d in dims:
+        try:
+            name, size = d
+        except (TypeError, ValueError):
+            name, size = utils.RandName(), d
+        sh.append(mtf.Dimension(name, size))
+
+    sh = mtf.Shape(sh)
+    return sh
+
+def CreateMeshes(inputs, labels, num_nodes, num_gpus, batch_size):
+    graph = mtf.Graph()
+    meshes = []
+    mesh_to_impl = {}
+
+    assert num_gpus % num_nodes == 0
+    gpus_per_node = num_gpus // num_nodes
+
+    def GetMeshImpl(dev_cnts, devices=None, node_cnt=num_nodes):
+        assert ((utils.RoundUp(utils.Prod(dev_cnts), gpus_per_node)) ==
+                (gpus_per_node * num_nodes))
+        return utils.GetMeshImpl(dev_cnts, devices=devices, num_nodes=node_cnt)
+
+    mesh = mtf.Mesh(graph, 'mesh0')
+    meshes.append(mesh)
+    mesh_to_impl[mesh] = GetMeshImpl([num_gpus])
+
+    assert len(inputs.shape) == 2
+    assert inputs.shape == labels.shape
+
+    shape = GetShape([('axis0', batch_size), inputs.get_shape().as_list()[1]])
+    mtf_inputs = mtf.import_tf_tensor(mesh, inputs, shape)
+    mtf_labels = mtf.import_tf_tensor(mesh, labels, shape)
+
+    return graph, meshes, mesh_to_impl, mtf_inputs, mtf_labels
 
 class LSTMCell(keras.layers.Layer):
     def __init__(self, batch_size, num_units, layer, **kwargs):
@@ -36,34 +77,56 @@ class LSTMCell(keras.layers.Layer):
 def model(params, inputs, labels):
     devices = params.devices
     num_gpus = len(devices)
+    learning_rate = 0.01
 
-    embedding = keras.layers.Embedding(params.vocab_size, params.num_units,
-            input_length=params.max_seq_len)
+    # RNN cells
     cells = [LSTMCell(params.batch_size, params.num_units, layer=0),
             LSTMCell(params.batch_size, params.num_units, layer=1)]
     rnn = keras.layers.RNN(cells, return_sequences=True, return_state=False)
-    dense = keras.layers.Dense(params.vocab_size, use_bias=False)
 
-    assert params.batch_size % num_gpus == 0
-    stride = params.batch_size // num_gpus
-    start, end = 0, stride
-    ys = []
-    for dev in devices:
-        with tf.device(dev):
-            ys.append(dense(rnn(embedding(inputs[start:end, ...]))))
-            start = end
-            end += stride
-    assert start == params.batch_size
+    # Mtf mesh
+    assert len(inputs.shape) == 2
+    graph, meshes, mesh_to_impl, mtf_inputs, mtf_labels = CreateMeshes(
+            inputs, labels, params.num_nodes, num_gpus, params.batch_size)
 
-    # Loss
-    with tf.device(devices[0]):
-        y = tf.concat(ys, axis=0)
-        loss = tf.losses.sparse_softmax_cross_entropy(labels, y,
-                reduction=tf.losses.Reduction.MEAN)
-        opt = tf.train.GradientDescentOptimizer(learning_rate=0.01)
-    grads = opt.compute_gradients(loss, colocate_gradients_with_ops=True)
-    assert all(g is not None for g in grads)
-    grads = opt.apply_gradients(grads)
+    # Embedding dimensions
+    vocab_dim = mtf.Dimension(utils.RandName(), params.vocab_size)
+    embed_dim = mtf.Dimension(utils.RandName(), params.num_units)
 
-    return loss, grads
+    # Model
+    embedding = mtf.layers.embedding(mtf_inputs, vocab_dim, embed_dim,
+            tf.float32)
+    y = mtf.slicewise(rnn, [embedding], output_shape=embedding.shape,
+            output_dtype=embedding.dtype, splittable_dims=embedding.shape[:1])
+    y = mtf.layers.dense(y, vocab_dim, reduced_dims=y.shape[-1:],
+            use_bias=False)
+    mtf_cross_ent = mtf.layers.softmax_cross_entropy_with_logits(y, mtf_labels,
+            vocab_dim)
+    mtf_loss = mtf.reduce_mean(mtf_cross_ent)
+
+    # Optimize
+    with tf.variable_scope('optimize'):
+        grads = mtf.gradients([mtf_loss], [v.outputs[0] for v in
+            graph.trainable_variables])
+        opt = mtf.optimize.SgdOptimizer(learning_rate)
+        grad_updates = opt.apply_grads(grads, graph.trainable_variables)
+
+
+    print('Beginning to lower mtf graph...', flush=True)
+    lowering = mtf.Lowering(graph, mesh_to_impl)
+    print('Finished lowering.', flush=True)
+    tf_loss = lowering.export_to_tf_tensor(mtf_loss)
+    tf_grad_updates = [lowering.lowered_operation(op) for op in grad_updates]
+
+    # Initializer
+    tf_init_vars = utils.FlattenList([
+                lowering.variables[var].laid_out_tensor.all_slices for var in
+                graph.trainable_variables])
+    init_op = tf.trainable_variables()
+    for v in tf_init_vars:
+        with tf.device(v.device):
+            init_op.append(v.initializer)
+    print(init_op)
+
+    return init_op, tf_loss, tf_grad_updates
 
